ae64159026c39ea43ada2643e1578399993fa806
jjunell <jaime.junell@gmail.com>
2015-06-26 14:48:35 +0200

    debugging printfs

--- sw/airborne/firmwares/rotorcraft/guidance/guidance_v.c
+++ sw/airborne/firmwares/rotorcraft/guidance/guidance_v.c
@@ -405,6 +405,7 @@ static void run_hover_loop(bool_t in_flight)
   guidance_v_ff_cmd = g_m_zdd / inv_m;
   /* feed forward command */
   guidance_v_ff_cmd = (guidance_v_ff_cmd << INT32_TRIG_FRAC) / guidance_v_thrust_coeff;
+/*printf("ff_cmd = %d \n", guidance_v_ff_cmd);*/
 
   /* bound the nominal command to 0.9*MAX_PPRZ */
   Bound(guidance_v_ff_cmd, 0, 8640);
